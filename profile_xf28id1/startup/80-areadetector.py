45bdb3cce8e8417362a19ac9427b06c68910aa1f
XPD Operator <xf28id1@xf28id1-ws2.cs.nsls2.local>
2015-03-18 17:36:49 -0400

    Add PE detectors pe1, pe2

---
+++ profile_xf28id1/startup/80-areadetector.py
@@ -0,0 +1,21 @@
+from ophyd.controls.area_detector import (AreaDetectorFileStoreHDF5,
+                                          AreaDetectorFileStoreTIFF)
+
+shctl1 = EpicsSignal('XF:28IDC-ES:1{Det:PE1}cam1:ShutterMode', name='shctl1')
+shctl2 = EpicsSignal('XF:28IDC-ES:1{Det:PE2}cam1:ShutterMode', name='shctl2')
+
+pe1 = AreaDetectorFileStoreTIFF('XF:28IDC-ES:1{Det:PE1}', name='pe1',
+                                stats=[],
+                                ioc_file_path = 'X:/pe1_data',
+                                file_path = '/home/xf28id1/pe1_data',
+                                shutter=shctl1,
+                                shutter_val=(1,0))
+
+
+pe2 = AreaDetectorFileStoreTIFF('XF:28IDC-ES:1{Det:PE2}', name='pe2',
+                                stats=[],
+                                ioc_file_path = 'Z:/pe2_data',
+                                file_path = '/home/xf28id1/pe2_data',
+                                shutter=shctl2,
+                                shutter_val=(1,0))
+
