5869f440a9992a23d8440bfc82e4463d0c5be2d2
XPD Operator <xf28id1@xf28id1-ws2.cs.nsls2.local>
2015-03-18 17:33:37 -0400

    Add cs700 positioner for the CryoStream cooler.

--- profile_xf28id1/startup/10-endstation.py
+++ profile_xf28id1/startup/10-endstation.py
@@ -1,5 +1,9 @@
-from ophyd.controls import EpicsMotor
+from ophyd.controls import EpicsMotor, PVPositioner
 
 
 tth = EpicsMotor('XF:28IDC-ES:1{Dif-Ax:2ThI}Mtr', name='tth')
+
+cs700 = PVPositioner('XF:28IDC-ES:1{Env:01}T-SP',
+                     readback='XF:28IDC-ES:1{Env:01}T-I',
+                     put_complete=True)
 
