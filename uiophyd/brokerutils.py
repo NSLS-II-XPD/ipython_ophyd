2d402e2795e06f5aa350d4834dfd7e40bb9d90f9
Pavol Juhas <pavol.juhas@gmail.com>
2015-03-23 16:29:36 -0400

    Add functions ui_imagearray, ui_nonzerofraction.

--- uiophyd/brokerutils.py
+++ uiophyd/brokerutils.py
@@ -21,3 +21,19 @@ def events_generator(header):
     for e in blank_events(header):
         fill_event(e)
         yield e
+
+
+def ui_imagearray(header, index=0):
+    "Return image array from the header object and event index."
+    e = blank_events(header)[index]
+    fill_event(e)
+    nm = [k for k in e.data if k.endswith('image_lightfield')][0]
+    rv = e.data[nm][0]
+    return rv
+
+
+def ui_nonzerofraction(header, index=0):
+    x = ui_imagearray(header, index)
+    cnz = (x.flatten() != 0).sum()
+    rv = float(cnz) / x.size
+    return rv
