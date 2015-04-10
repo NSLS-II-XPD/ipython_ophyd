fe1d6f1dabf3a77fef2447af4efeb4a14510894d
Kevin Knox <kevinrknox@gmail.com>
2015-04-10 15:52:37 -0400

    Added functionality for working with gas by name in gas_switcher

--- profile_xf28id1/startup/gas_switcher.py
+++ profile_xf28id1/startup/gas_switcher.py
@@ -20,27 +20,64 @@ class GasSwitcher(SignalGroup):
 
 
 class XPDGasSwitcher(GasSwitcher):
-    def __init__(self, requested_pos=None, current_pos=None):
+    def __init__(self, requested_pos=None, current_pos=None, gasdict = {}):
        GasSwitcher.__init__(self)
        signals = [EpicsSignal(current_pos, rw=False, alias='_current_pos'),
                   EpicsSignal(requested_pos, alias='_requested_pos'),
                   ] 
-
+       self._gasdict = gasdict
        for sig in signals:
            self.add_signal(sig)
+       return
+
 
     @property
     def current_pos(self):
         return self._current_pos.value
 
+
     @property
     def requested_pos(self):
         return self._requested_pos.value
 
+
     @requested_pos.setter
     def requested_pos(self, val):
         self._requested_pos.value = val
+        return
 
 
-gas1 = XPDGasSwitcher(requested_pos='XF:28IDC-ES:1{Env:02}Pos-SP',
-        current_pos='XF:28IDC-ES:1{Env:02}Pos-I')
+    def define_gas(self, gas, n):
+        self._gasdict[n] = gas
+        return
+
+
+    def print_gasses(self):
+        print('Gas positions are defined as follows:')
+        print('-------------------------------------')
+        for k, v in self._gasdict.items():
+            print(k, '-', v)
+        print('-------------------------------------')
+        print('switch(\'gas\') -> switch to \'gas\'')
+        print('define_gas(\'gas\', n) -> define \'gas\' to be at position n')
+        return
+
+
+    def switch(self, newgas):
+        if not newgas in self._gasdict.values():
+            print(gas, 'has not been defined.')
+            return
+        gas_nums = [n for n, gas in self._gasdict.items() if gas == newgas]
+        if len(gas_nums) > 1:
+            print('Error: more than one position has been defined for this gas')
+            return
+        self.requested_pos = gas_nums[0]
+        return
+
+
+    def print_curr_gas(self): 
+        print('Current gas is:')
+        print(self._gasdict[self.current_pos])
+        return
+
+
