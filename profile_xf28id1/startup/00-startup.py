86ce6b11ab3f483ef1257259490217943c1b6862
Kevin Knox <kevinrknox@gmail.com>
2015-04-10 16:30:25 -0400

    ENH: Add DataBroker imports to ophyd startup

--- profile_xf28id1/startup/00-startup.py
+++ profile_xf28id1/startup/00-startup.py
@@ -1,3 +1,6 @@
 import logging
 session_mgr._logger.setLevel(logging.CRITICAL)
 from ophyd.userapi import *
+from dataportal import (DataBroker as db, 
+                        StepScan as ss, DataBroker, 
+                        StepScan, DataMuxer)
