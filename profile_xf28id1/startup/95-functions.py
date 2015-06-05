33db7fc947087b5b0a6582d9e4f7f32ffad5b82f
Pavol Juhas <pavol.juhas@gmail.com>
2015-06-05 17:37:59 -0400

    Remove save_tiff function.
    Use the TiffSaver class instead.

--- profile_xf28id1/startup/95-functions.py
+++ profile_xf28id1/startup/95-functions.py
@@ -27,7 +27,7 @@ def show_last():
 
 def show(light_id, dark_id=None):
     """Show light/dark/difference based on two scan ids
-    if no dark_id is provided, it is assumed that the dark_id 
+    if no dark_id is provided, it is assumed that the dark_id
     was collected first
     """
     light = ss[light_id]['pe1_image_lightfield'][0].astype(float)
@@ -70,89 +70,9 @@ def _show_image(light, title):
 def _show_subtracted(light, dark):
     _show_image(light,'pe1_image_lightfield' )
     _show_image(dark, 'pe1_image_darkfield')
-    subtracted = light - dark 
+    subtracted = light - dark
     _show_image(subtracted, 'light - dark')
 
-def save_tiff_temporary(light_id, filepath, fields):
-    light = ss[light_id]['pe1_image_lightfield']
-    dark = ss[light_id-1]['pe1_image_lightfield']
-    
-    _save_tiff_helper(light, dark, filepath, fields, '%d&%d' % (light_id-1,
-        light_id))
-
-
-def save_tiff(scan_ids, filepath, fields):
-    """
-    Parameters
-    ----------
-    scan_id : int, list
-        List of scan ids you wish to output to tiffs
-    filepath : str
-        directory to save TIFF files in
-    fields : list
-        List of fields in the run header to include in the 
-        file name
-
-    Example
-    -------
-
-    >>> save_tiff(187, '~/my_folder/', ['mood', 'sample_flavor'])
-    '~/my_folder/scan-187_mood-excited_sample_flavor-orange-0.tif'
-
-
-    """
-    try:
-        iter(scan_ids)
-    except TypeError:
-        # scan_id is not iterable and therefore not a list
-        scan_ids = [scan_ids]
-    for scan_id in scan_ids:
-        filepath = os.path.expanduser(filepath)
-        df = ss[scan_id]
-        header = db[scan_id]
-        light = df['pe1_image_lightfield']
-        try:
-            dark = df['pe1_image_darkfield']
-        except KeyError:
-            print("No dark frame available for scan %d. Only outputting the light"
-                "field" % scan_id)
-        try:
-            _save_tiff_helper(light, dark, header, filepath, fields, scan_id)
-        except:
-            print("There was a problem with scan %s. Traceback: " % scan_id)
-            print(traceback.format_exc())
-
-
-def _save_tiff_helper(light, dark, header, filepath, fields, scan_id):
-    data = {'light': light, 'dark': dark, 'subtracted': light-dark} 
-    full_filepaths = []
-    for img_name, images in data.items():
-        for i, img in enumerate(images):
-            filename = "scan-%s" % scan_id
-            for field in fields:
-                # Look up each field in the Header.
-                try:
-                    value = header[field]
-                except KeyError:
-                    raise KeyError("The field %s was not in the Header "
-                                   "for scan %d. It has fields: %s " % (field, scan_id,
-                                       fields))
-                filename += "_%s-%s" % (field, value)
-            if img_name != 'subtracted':
-                filename += "_%s" % img_name
-            filename += "_%05d.tif" % i
-            full_filepath = os.path.join(filepath, filename)
-            print('Writing to %s' % full_filepath), 
-            tifffile.imsave(full_filepath, img)
-            print(' ...written')
-            full_filepaths.append(full_filepath)
-    return full_filepaths
-
-
-#def save_jon_tiff(scan_id, filepath, fields=None):
-#    if fields is None:
-#        fields = [...]  # <-- your defaults here
-#    return save_tiff(scan_id, filepath, fields)
 
 def gas_macro(gas, ct, old_gas, new_gas, duration, *args, **kwargs):
     """For Jon
@@ -217,7 +137,7 @@ def gas_macro2(gas, ct, old_gas, new_gas, duration, *args, **kwargs):
     # such as metadata.
     >>> gas_macro('N2', 'O2', 600, my_meta_field='blah')
     """
-    # start with first gas 
+    # start with first gas
     gas.switch(old_gas)
     while gas.requested_pos != gas.current_pos:
         time.sleep(0.5)
@@ -247,4 +167,3 @@ def gas_macro2(gas, ct, old_gas, new_gas, duration, *args, **kwargs):
         kwargs['gasses'] = gas.lookup
         ct(*args, **kwargs)
     return
-
