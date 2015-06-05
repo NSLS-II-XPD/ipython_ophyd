55bca9c457ca64dfc998bd5d5d4b27d4c47a87e3
Pavol Juhas <pavol.juhas@gmail.com>
2015-06-05 14:30:34 -0400

    Import matplotlib after dataportal.
    Avoid issues with incompatible QT initializations.

--- profile_xf28id1/startup/90-imports.py
+++ profile_xf28id1/startup/90-imports.py
@@ -1,6 +1,5 @@
-import os
-if os.environ.get('DISPLAY', 0):
-    from matplotlib.pyplot import *
+#!/usr/bin/env python
+
 
 from uiophyd.tiffsaver import TiffSaver
 from uiophyd.brokerutils import blank_events
@@ -8,3 +7,8 @@ from uiophyd.brokerutils import ui_imagearray, ui_nonzerofraction
 
 # create a default TiffSaver object ts:
 ts = TiffSaver()
+
+# Import pyplot after the dataportal implied in brokerutils.
+import os
+if os.environ.get('DISPLAY', 0):
+    from matplotlib.pyplot import *
