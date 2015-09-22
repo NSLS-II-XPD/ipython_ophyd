dffb966e7946a15e3a238f3b9447d1eaeada7083
Pavol Juhas <pavol.juhas@gmail.com>
2015-09-22 11:45:45 -0400

    Configure few detectors for the bluesky engine.

--- profile_collection/startup/00-startup.py
+++ profile_collection/startup/00-startup.py
@@ -3,3 +3,11 @@ session_mgr._logger.setLevel(logging.INFO)
 from dataportal import (DataBroker as db,
                         StepScan as ss, DataBroker,
                         StepScan, DataMuxer)
+
+
+from bluesky.standard_config import *
+
+gs.RE.md['config'] = {}
+gs.RE.md['owner'] = 'xf28id1'
+gs.RE.md['group'] = 'XPD'
+gs.RE.md['beamline_id'] = 'xpd'
--- profile_collection/startup/50-scans.py
+++ profile_collection/startup/50-scans.py
@@ -1,3 +1,2 @@
 # Configure bluesky default detectors with this:
-# ascan.default_triggers = [em_cnt]
-# ascan.default_detectors = [em_ch1, em_ch2, em_ch3, em_ch4]
+gs.DETS = [em_ch1, em_ch2, em_ch3, em_ch4]
