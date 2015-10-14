deea5285907f91ac75ca82918f357f882610c78b
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-14 11:49:49 -0400

    Reimplement em* objects as one EpicsScalar. Ditto det_sc.

--- profile_collection/startup/20-detectors.py
+++ profile_collection/startup/20-detectors.py
@@ -1,13 +1,20 @@
-from ophyd.controls import EpicsSignal, PerkinElmerDetector
+from ophyd.controls import EpicsSignal, PerkinElmerDetector, EpicsScaler
 
 
-em_cnt = EpicsSignal('XF:28IDC-BI:1{IM:02}.CNT', name='em_cnt')
+# em_cnt = EpicsSignal('XF:28IDC-BI:1{IM:02}.CNT', name='em_cnt')
 # write 1 for AutoCount, 0 for OneShot
-em_mode = EpicsSignal('XF:28IDC-BI:1{IM:02}.CONT', name='em_mode')
-em_ch1 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S20', rw=False, name='em_ch1')
-em_ch2 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S21', rw=False, name='em_ch2')
-em_ch3 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S23', rw=False, name='em_ch3')
-em_ch4 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S23', rw=False, name='em_ch4')
+# em_mode = EpicsSignal('XF:28IDC-BI:1{IM:02}.CONT', name='em_mode')
+# em_ch1 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S20', rw=False, name='em_ch1')
+# em_ch2 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S21', rw=False, name='em_ch2')
+# em_ch3 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S22', rw=False, name='em_ch3')
+# em_ch4 = EpicsSignal('XF:28IDC-BI:1{IM:02}.S23', rw=False, name='em_ch4')
+em = EpicsScaler('XF:28IDC-BI:1{IM:02}', chan_start=20, numchan=4, name='em',
+                 preset_pv=False, gate_pv=False)
+
+# Energy Calibration Scintillator
+det_sc2 = EpicsScaler('XF:28IDC-ES:1{Det:SC2}.S2', chan_start=2, numchan=2,
+                      preset_pv=False, gate_pv=False, name='det_sc2')
+       
 
 # Perkin Elmer aSi x-ray detector
 pe_cam = PerkinElmerDetector('XF:28IDC-ES:1{Det:PE1}')
