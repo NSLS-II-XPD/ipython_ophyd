24e65db624221d559f46ce74d88ad28ec970d754
Pavol Juhas <pavol.juhas@gmail.com>
2015-10-14 11:49:03 -0400

    Update bluesky's API to the qt_kicker.

--- profile_collection/startup/00-startup.py
+++ profile_collection/startup/00-startup.py
@@ -12,3 +12,8 @@ gs.RE.md['config'] = {}
 gs.RE.md['owner'] = 'xf28id1'
 gs.RE.md['group'] = 'XPD'
 gs.RE.md['beamline_id'] = 'xpd'
+
+
+import bluesky.qt_kicker
+bluesky.qt_kicker.install_qt_kicker()
+
