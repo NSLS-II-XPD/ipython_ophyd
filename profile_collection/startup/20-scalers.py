1be5d54e49931f1badd838eafd7d88a5eb6dd06d
Pavol Juhas <pavol.juhas@gmail.com>
2016-01-12 16:03:28 -0500

    Fix scaler logic

--- profile_collection/startup/20-scalers.py
+++ profile_collection/startup/20-scalers.py
@@ -1,16 +1,8 @@
 from ophyd import EpicsScaler
 
 
-class PartialEpicsScaler(EpicsScaler):
-    """
-    This scaler does not implement the 'gates' portion
-    of the standard EPICS scaler record, so we simply
-    override it here with 'None'.
-    """
-    gates = None
-
-
-em = PartialEpicsScaler('XF:28IDC-BI:1{IM:02}', name='em')
+em = EpicsScaler('XF:28IDC-BI:1{IM:02}', name='em')
+em.channels.read_attrs = ['chan%d' % i for i in [20, 21, 22, 23]]
 
 # Energy Calibration Scintillator
-det_sc2 = PartialEpicsScaler('XF:28IDC-ES:1{Det:SC2}', name='det_sc2')
+det_sc2 = EpicsScaler('XF:28IDC-ES:1{Det:SC2}', name='det_sc2')
