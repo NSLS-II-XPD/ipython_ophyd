af79fb38f94754b5a577e51122fc38fe210196ae
Pavol Juhas <pavol.juhas@gmail.com>
2016-01-12 16:03:19 -0500

    Clean up imports

--- profile_collection/startup/00-startup.py
+++ profile_collection/startup/00-startup.py
@@ -1,14 +1,17 @@
 import logging
-from databroker import DataBroker as db
 
+# Import these things for convenience.
+from databroker import DataBroker as db, get_images, get_table, get_events
+import ophyd
+from ophyd.commands import *
 
 from bluesky.standard_config import *
-from ophyd.commands import *
 
 gs.RE.md['owner'] = 'xf28id1'
 gs.RE.md['group'] = 'XPD'
 gs.RE.md['beamline_id'] = 'xpd'
 
-
+# This makes the plots update live.
 import bluesky.qt_kicker
 bluesky.qt_kicker.install_qt_kicker()
+del bluesky.qt_kicker
