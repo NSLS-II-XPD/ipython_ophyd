2fdb57d01cb9455bfb9d7d8fb41d9e586da86013
Dan Allan <dallan@bnl.gov>
2016-01-27 13:41:46 -0500

    more minor updates

--- profile_collection/startup/11-temperature-controller.py
+++ profile_collection/startup/11-temperature-controller.py
@@ -1,19 +1,22 @@
 from ophyd import PVPositioner, EpicsSignal, EpicsSignalRO
 from ophyd import Component as C
+from ophyd.device import DeviceStatus
 
 
 class CS700TemperatureController(PVPositioner):
-    setpoint = C(EpicsSignal, 'T-I.VAL')
-    readback = C(EpicsSignalRO, 'T-I.RBV')
+    setpoint = C(EpicsSignal, 'T-SP')
+    readback = C(EpicsSignalRO, 'T-I')
     done = C(EpicsSignalRO, 'Cmd-Busy')
     stop_signal = C(EpicsSignal, 'Cmd-Cmd')
 
-cs700 = CS700TemperatureController('XF:28IDC-ES:1{Env:01}', name='cs700')
+    def trigger(self):
+        # There is nothing to do. Just report that we are done.
+        # Note: This really should not necessary to do --
+        # future changes to PVPositioner may obviate this code.
+        status = DeviceStatus()
+        status._finished()
+        return status
 
-
-# old version from ophyd v0.1.x
-# cs700 = PVPositioner('XF:28IDC-ES:1{Env:01}'),
-#                      readback='XF:28IDC-ES:1{Env:01}T-I',
-#                     #done='XF:28IDC-ES:1{Env:01}Cmd-Busy', done_val=0,
-#                     stop='XF:28IDC-ES:1{Env:01}Cmd-Cmd', stop_val=13,
-#                     put_complete=True, name='cs700')
+cs700 = CS700TemperatureController('XF:28IDC-ES:1{Env:01}', name='cs700',
+                                   settle_time=10)
+cs700.done_value = 0
