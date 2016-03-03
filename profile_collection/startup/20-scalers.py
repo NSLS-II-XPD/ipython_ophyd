f707f14c7fcad90e9c3f7b5365ad49390d7bc389
Pavol Juhas <pavol.juhas@gmail.com>
2016-03-03 11:38:13 -0500

    rename scaler channels
    Restore the names to what they used to be

--- profile_collection/startup/20-scalers.py
+++ profile_collection/startup/20-scalers.py
@@ -3,6 +3,9 @@ from ophyd import EpicsScaler
 
 em = EpicsScaler('XF:28IDC-BI:1{IM:02}', name='em')
 em.channels.read_attrs = ['chan%d' % i for i in [20, 21, 22, 23]]
+for ch_name in em.channels.signal_names:
+    ch = getattr(em.channels, ch_name)
+    ch.name = ch.name.replace('_channels_', '_')
 
 # Energy Calibration Scintillator
 det_sc2 = EpicsScaler('XF:28IDC-ES:1{Det:SC2}', name='det_sc2')
