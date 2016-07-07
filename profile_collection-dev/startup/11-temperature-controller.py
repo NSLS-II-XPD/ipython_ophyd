d22653f6e87633bc228388384e26d93fa6a46aaf
XPD Operator <xf28id1@xf28id1-ws2.cs.nsls2.local>
2016-07-07 15:58:06 -0400

    Put collection-dev under version control

---
+++ profile_collection-dev/startup/11-temperature-controller.py
@@ -0,0 +1,36 @@
+from ophyd import PVPositioner, EpicsSignal, EpicsSignalRO
+from ophyd.mixins import EpicsSignalPositioner
+from ophyd import Component as C
+from ophyd.device import DeviceStatus
+
+
+class CS700TemperatureController(PVPositioner):
+    readback = C(EpicsSignalRO, 'T-I')
+    setpoint = C(EpicsSignal, 'T-SP')
+    done = C(EpicsSignalRO, 'Cmd-Busy')
+    stop_signal = C(EpicsSignal, 'Cmd-Cmd')
+    
+    def set(self, *args, timeout=None, **kwargs):
+        return super().set(*args, timeout=timeout, **kwargs)
+
+    def trigger(self):
+        # There is nothing to do. Just report that we are done.
+        # Note: This really should not necessary to do --
+        # future changes to PVPositioner may obviate this code.
+        status = DeviceStatus(self)
+        status._finished()
+        return status
+
+# To allow for sample temperature equilibration time, increase
+# the `settle_time` parameter (units: seconds).
+cs700 = CS700TemperatureController('XF:28IDC-ES:1{Env:01}', name='cs700',
+                                   settle_time=0)
+cs700.done_value = 0
+cs700.read_attrs = ['setpoint', 'readback']
+cs700.readback.name = 'temperature'
+cs700.setpoint.name = 'temperature_setpoint'
+
+
+eurotherm = EpicsSignalPositioner('XF:28IDC-ES:1{Env:04}T-I',
+                                  write_pv='XF:28IDC-ES:1{Env:04}T-SP',
+                                  tolerance=10, name='eurotherm')
---
+++ profile_collection-dev/startup/999-load.py
@@ -0,0 +1,36 @@
+import os
+
+from bluesky.callbacks import LiveTable
+
+from xpdacq.new_xpdacq.glbl import glbl
+from xpdacq.new_xpdacq.beamtimeSetup import (start_xpdacq, _start_beamtime,
+                                             _end_beamtime)
+from xpdacq.new_xpdacq.beamtime import *
+
+
+try:
+    # if pe1c and other exits, i.e. at XPD 
+    glbl.area_det = pe1c
+    glbl.shutter = shctl1
+    glbl.temp_controller = cs700
+except NameError:
+    pass
+
+from xpdacq.new_xpdacq.xpdacq import *
+# beamtime reload happen in xpdacq
+
+# gonna seperate analysis from collection
+#from xpdacq.analysis import * 
+
+HOME_DIR = glbl.home
+BASE_DIR = glbl.base
+YAML_DIR = glbl.yaml_dir
+
+print('INFO: Initializing the XPD data acquisition environment')
+if os.path.isdir(HOME_DIR):
+    os.chdir(HOME_DIR)
+else:
+    os.chdir(BASE_DIR)
+
+print('OK, ready to go.  To continue, follow the steps in the xpdAcq')
+print('documentation at http://xpdacq.github.io/xpdacq')
