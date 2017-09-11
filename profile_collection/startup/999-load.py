7ec00e686523932c712671f5db5b45f012b52ac6
XPD Operator <xf28id1@xf28id1-ws2.cs.nsls2.local>
2017-09-11 10:22:53 -0400

    MNT: uncommited local edits at XPD

--- profile_collection/startup/999-load.py
+++ profile_collection/startup/999-load.py
@@ -15,16 +15,19 @@
 ##############################################################################
 import os
 from xpdacq.xpdacq_conf import (glbl_dict, configure_device,
-                                _reload_glbl, _set_glbl)
+                                _reload_glbl, _set_glbl,
+                                _load_beamline_config)
 
 # configure experiment device being used in current version
 if glbl_dict['is_simulation']:
-    from xpdacq.simulation import pe1c, db, cs700, shctl1
-    configure_device(area_det=pe1c, shutter=shctl1,
+    from xpdacq.simulation import xpd_pe1c, db, cs700, shctl1
+
+    configure_device(area_det=xpd_pe1c, shutter=shctl1,
                      temp_controller=cs700, db=db)
 else:
-    #FIXME: create synthetic ring current object in fullness of time
+    # FIXME: create synthetic ring current object in fullness of time
     from ophyd import EpicsSignalRO
+
     ring_current = EpicsSignalRO('SR:OPS-BI{DCCT:1}I:Real-I',
                                  name='ring_current')
     configure_device(area_det=pe1c, shutter=shctl1,
@@ -38,6 +41,7 @@ from xpdacq.glbl import glbl
 # reload beamtime
 from xpdacq.beamtimeSetup import (start_xpdacq, _start_beamtime,
                                   _end_beamtime)
+
 bt = start_xpdacq()
 if bt is not None:
     print("INFO: Reload beamtime objects:\n{}\n".format(bt))
@@ -51,12 +55,14 @@ from xpdacq.utils import import_sample_info
 
 # instantiate xrun without beamtime, like bluesky setup
 xrun = CustomizedRunEngine(None)
-xrun.md['owner'] = glbl['owner']
 xrun.md['beamline_id'] = glbl['beamline_id']
 xrun.md['group'] = glbl['group']
+xrun.md['facility'] = glbl['facility']
+beamline_config = _load_beamline_config(glbl['blconfig_path'])
+xrun.md['beamline_config'] = beamline_config
 
 # insert header to db, either simulated or real
-xrun.subscribe('all', db.mds.insert)
+xrun.subscribe(db.insert, 'all')
 
 if bt:
     xrun.beamtime = bt
@@ -73,7 +79,7 @@ else:
 from xpdacq.calib import *
 
 # analysis functions, only at beamline
-from xpdan.data_reduction import *
+# from xpdan.data_reduction import *
 
 print('OK, ready to go.  To continue, follow the steps in the xpdAcq')
 print('documentation at http://xpdacq.github.io/xpdacq\n')
