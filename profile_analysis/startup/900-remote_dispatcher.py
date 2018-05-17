ba25b86eb793b8a29c4a6aefda384df68fe57e28
christopher <cjwright4242@gmail.com>
2018-05-17 10:54:25 -0400

    ENH: start_analysis function

--- profile_analysis/startup/900-remote_dispatcher.py
+++
@@ -1,7 +0,0 @@
-from bluesky.callbacks.zmq import RemoteDispatcher
-from bluesky.utils import install_qt_kicker
-
-
-d = RemoteDispatcher('10.28.0.202:5578')
-install_qt_kicker(loop=d.loop)  # This may need to be d._loop depending on tag
-
