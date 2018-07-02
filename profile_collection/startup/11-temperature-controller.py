9481c09865d0c8fe0475bf402527472e1cc78a6a
Julien Lhermitte <lhermitte@bnl.gov>
2018-07-02 11:40:13 -0400

    Added furnace

--- profile_collection/startup/11-temperature-controller.py
+++ profile_collection/startup/11-temperature-controller.py
@@ -108,3 +108,35 @@ class CryoStat(Device):
 
 
 cryostat = CryoStat('XF:28IDC_ES1:LS335:{CryoStat}', name='cryostat', dead_band=1)
+
+
+# TODO : PV needs to be fixed for done signal
+# (doesn't work on ramp down)
+class LinkamFurnace(PVPositioner):
+    readback = C(EpicsSignalRO, 'RAMP:LIMIT:SET.VAL')
+    setpoint = C(EpicsSignal, 'RAMP:LIMIT:SET')
+    done = C(EpicsSignalRO, 'STATUS')
+    stop_signal = C(EpicsSignal, 'RAMP:CTRL:SET')
+    temperature = C(EpicsSignal, "TEMP")
+
+    def set(self, *args, timeout=None, **kwargs):
+        return super().set(*args, timeout=timeout, **kwargs)
+
+    def trigger(self):
+        # There is nothing to do. Just report that we are done.
+        # Note: This really should not necessary to do --
+        # future changes to PVPositioner may obviate this code.
+        status = DeviceStatus(self)
+        status._finished()
+        return status
+
+# To allow for sample temperature equilibration time, increase
+# the `settle_time` parameter (units: seconds).
+linkam_furnace = LinkamFurnace('XF:28IDC-ES:2:{LINKAM}:', name='cs700',
+                                   settle_time=0)
+linkam_furnace.done_value = 3
+linkam_furnace.stop_value = 0
+linkam_furnace.setpoint.kind = "normal"
+linkam_furnace.readback.kind = "normal"
+linkam_furnace.readback.name = 'temperature'
+linkam_furnace.setpoint.name = 'temperature_setpoint'
